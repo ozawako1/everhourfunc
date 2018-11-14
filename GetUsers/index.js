6cbc086d778132c42d58f78c1b8808ff30dcdb87
ozawako1 <ozawako1@gmail.com>
2018-11-14 09:31:48 +0900

    get groups

--- GetUsers/index.js
+++ GetUsers/index.js
@@ -1,6 +1,26 @@
 const HTTP = require('request');
 const API_TOKEN = process.env['API_TOKEN'];
 
+const TYPE_USER = "user";
+const TYPE_GROUP = "group";
+
+function format_groups(arr)
+{
+    //所属しているユーザー情報を切り捨てる
+    var groups = [];
+
+    arr.forEach(function(itm){
+        var g = {
+            'id': itm.id,
+            'name': itm.name
+        }
+        groups.push(g);
+
+    });
+
+    return groups;
+}
+
 function format_users(arr)
 {
     var users = [];
@@ -27,6 +47,34 @@ function format_users(arr)
     });
 
     return users;
+}
+
+function EH_get_groups(callback)
+{
+    var ret = 0;
+
+    var headers = {
+        'Content-Type': 'application/json',
+        'X-Api-Key': API_TOKEN
+    };
+
+    var options = {
+        'url': 'https://api.everhour.com/team/groups',
+        'method': 'GET',
+        'headers': headers,
+        'json': true,
+    };
+
+    HTTP(options, function(error, response, body) {
+        if (error) {
+          callback(error, null);
+        } else {
+          var users = format_groups(body);
+          callback(null, users);
+        }
+    });
+    
+    return ret;
 }
 
 function EH_get_users(callback)
@@ -58,23 +106,51 @@ function EH_get_users(callback)
 };
 
 module.exports = function (context, req) {
+    
     context.log('JavaScript HTTP trigger function processed a request.');
-    context.log('API_TOKEN:'+ API_TOKEN);
-    EH_get_users(function(error, users){
-        if(error) {
-            context.log(error);
-            context.res = {
-                status: 500,
-                body: error.message
+
+    var type = TYPE_USER;
+
+
+    if (req.query != null && req.query.type == "group") {
+        type = TYPE_GROUP;
+    }
+
+    if (type == TYPE_USER){
+        EH_get_users(function(error, users){
+            if(error) {
+                context.log(error);
+                context.res = {
+                    status: 500,
+                    body: error.message
+                }
+                context.done();
+            } else {
+                context.res = {
+                    status: 200,
+                    body: users
+                }
+                context.done();
             }
-            context.done();
-        } else {
-            context.res = {
-                status: 200,
-                body: users
+        });
+    } else {
+        EH_get_groups(function(error, users){
+            if(error) {
+                context.log(error);
+                context.res = {
+                    status: 500,
+                    body: error.message
+                }
+                context.done();
+            } else {
+                context.res = {
+                    status: 200,
+                    body: users
+                }
+                context.done();
             }
-            context.done();
-        }
-    });
+        });
+    }
+    
 
 };
